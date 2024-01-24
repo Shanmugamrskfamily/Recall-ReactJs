ff45264a74a018d43ae49d1881d36262c8de88c8
Shanmugamrskfamily <shanmugamrskfamily@email.com>
2024-01-24 17:26:48 +0530

    Theme update

--- 24_01_2024/context-api-2/src/Header.jsx
+++ 24_01_2024/context-api-2/src/Header.jsx
@@ -3,6 +3,8 @@ import { ThemeContext } from './ThemeContext';
 
 function Header() {
     const {theme}=useContext(ThemeContext);
+    document.body.style.background = theme === 'light' ? '#eee' : '#333';
+    document.body.style.color = theme === 'light' ? '#333' : '#eee';
   return (
     <header style={{ background: theme === 'light' ? '#eee' : '#333', color: theme === 'light' ? '#333' : '#eee' }}>
         <h1>Themed Header</h1>
