de45a38c963bdf3667f8e95125164999c57c91dc
Shanmugamrskfamily <shanmugamrskfamily@email.com>
2024-01-24 18:18:16 +0530

    useRef usage practised

---
+++ 24_01_2024/useref-1/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Count from './Count'
+
+function App() {
+
+  return (
+    <div style={{display:'flex',flexDirection:'column',alignContent:'center', alignItems:'center'}}>
+      <Count/>
+    </div>
+  )
+}
+
+export default App
---
+++ 24_01_2024/useref-1/src/Count.jsx
@@ -0,0 +1,27 @@
+import React,{useState,useRef} from 'react'
+
+function Count() {
+const [count,setCount]=useState(0);
+const countRef=useRef(0);
+const previousCount=useRef(0);
+
+const handleClick=()=>{
+    previousCount.current=countRef.current;
+    countRef.current+=1;
+    setCount(count+1);
+    console.log('Current Ref: ',countRef.current);
+    console.log('Previous Ref: ',previousCount.current);
+    
+}
+  return (
+    <div>
+        <h1>Check Count</h1>
+        <p>Current Count by useRef: {countRef.current}</p>
+        <p>Previous Count by useRef: {previousCount.current}</p>
+        <p>Current Count by useState: {count}</p>
+        <button onClick={handleClick}>Increase Count</button>
+    </div>
+  )
+}
+
+export default Count
